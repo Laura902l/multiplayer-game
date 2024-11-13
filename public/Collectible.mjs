f298fa14a2062fa285b73e0f150a0f5b3d03f099
Laura <121919112+Laura902l@users.noreply.github.com>
2024-11-13 23:32:20 +0500

    Create Collectible.mjs

---
+++ public/Collectible.mjs
@@ -0,0 +1,27 @@
+
+class Collectible {
+  constructor({x, y, value, id}) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.id = id;
+    this.w = 10;
+    this.h = 10;
+  }
+
+  draw (context, img) {
+    context.drawImage(img, this.x, this.y, this.w, this.h)
+  }
+}
+
+/*
+  Note: Attempt to export this for use
+  in server.js
+*/
+try {
+  module.exports = Collectible;
+} catch(e) {
+  console.error(e)
+}
+
+export default Collectible;
